01299a5e0b7c298bfbd0f79d15f5bb7448f7e07e
Julieta <juliiy98@gmail.com>
2020-02-23 12:38:06 -0700

    Added attribute in Bill

--- src/main/java/com/spbootlogistic/bill/model/Bill.java
+++ src/main/java/com/spbootlogistic/bill/model/Bill.java
@@ -12,49 +12,49 @@ import java.time.LocalDate;
 public class Bill extends RepresentationModel<Bill> {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-
+    @Column
+    private Long claimId;
     @Column
     @NotNull(message = "folio fiscal cannot be empty")
     private String folioFiscal;
-
     @Column
     @NotNull(message = "transmitter RFC cannot be empty")
     private String transmitterRFC;
-
     @Column
     @NotNull(message = "transmitter name cannot be empty")
     private String transmitterName;
-
     @Column
     @NotNull(message = "receiver RFC cannot be empty")
     private String receiverRFC;
-
     @Column
     @NotNull(message = "receiver Name cannot be empty")
     private String receiverName;
-
     @Column
     @NotNull(message = "total cannot be empty")
     private double total;
-
     @Column
     //@NotNull(message = "date cannot be empty")
     @CreationTimestamp
     private LocalDate date;
-
     @Column
     @NotNull(message = "cfdi cannot be empty")
     private String cfdiUse;
-
     @Column
     @NotNull(message = "xmlUrl cannot be empty")
     private String xmlURL;
-
     @Column
     @NotNull(message = "pdfUrl cannot be empty")
     private String pdfURL;
+
+    public Long getClaimId() {
+        return claimId;
+    }
+
+    public void setClaimId(Long claimId) {
+        this.claimId = claimId;
+    }
 
     public long getId() {
         return id;
